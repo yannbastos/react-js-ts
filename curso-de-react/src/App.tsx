50615b3392ee900dd01434c19faa16805aa842d2
yannfbastos <yannfelipebastos52559@gmail.com>
2026-04-14 14:41:39 -0300

    feat: testing buttons

--- curso-de-react/src/App.tsx
+++ curso-de-react/src/App.tsx
@@ -1,3 +1,19 @@
+import { useState } from "react";
+
 export function App() {
-  return <>Olá</>;
+  const [count, setCount] = useState(0);
+  const [hide, setHide] = useState(false);
+
+  if (hide) return null;
+
+  return (
+    <div>
+      {hide && <p>Teste1</p>}
+      {!hide && <p>Teste2</p>}
+
+      {hide ? <p>Teste1</p> : <p>Teste2</p>}
+      <button onClick={() => setCount(count + 1)}>{count}</button>
+      <button onClick={() => setHide(!hide)}>Toggle</button>
+    </div>
+  );
 }
